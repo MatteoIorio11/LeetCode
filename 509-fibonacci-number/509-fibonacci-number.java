7cc7976fd0d3a8ef4624bf24bc2e3f831a0edbec
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-05 11:08:12 +0200

    Time: 1 ms (45.30%), Space: 39 MB (90.53%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -6,21 +6,9 @@ class Solution {
         if(n == 1){
             return 1;
         }
-        int[] arr = new int[n];
-        arr[0] = 0;
-        arr[1] = 1;
-        int f1 = 0;
-        int f2 = 1;
-        int f = 0;
-        for(int i = 0; i < n; i+=1){
-            System.out.println(i);
-            f = f1+f2;
-            var tmp = f1;
-            f1 = f;
-            f2 = tmp;
-            //f+=arr[i]+arr[i+1];
-            //arr[i+2] = f;
-        }
-        return f;
+        var phi1 = (1+Math.sqrt(5))/2;
+        var phi2 = (1-Math.sqrt(5))/2;
+        
+        return (int) ((Math.pow(phi1, n) - Math.pow(phi2, n))/Math.sqrt(5));
     }
 }
