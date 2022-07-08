a9bee33734c8d40c587972fca30bae9faea1d30e
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-08 12:08:11 +0200

    Time: 8 ms (76.37%), Space: 42.7 MB (72.04%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -3,18 +3,23 @@ class Solution {
         int cows = 0;
         int bulls = 0;
         int index = 0;
-        int[] map = new int[10];
+        Map<Character, Integer> map = new HashMap<>();
         
         for(;index < secret.length(); index++){
-            var s1 = secret.charAt(index) - '0';
-            var s2 = guess.charAt(index) - '0';
+            var s1 = secret.charAt(index);
+            var s2 = guess.charAt(index);
             if(s1 == s2){
                 bulls++;
             }else{
-                if(map[s1] < 0) {cows++;}
-                if(map[s2] > 0) {cows++;}
-                map[s1]++;
-                map[s2]--;
+                map.merge(s1, 1, (x,y) -> x+y);
+            }
+        }
+        for(index = 0; index < guess.length(); index++){
+            var s1 = secret.charAt(index);
+            var s2 = guess.charAt(index);
+            if(s1 != s2 && map.containsKey(s2) && map.get(s2) > 0){
+                cows++;
+                map.merge(s2, 1, (x,y) -> x-y);
             }
         }
         return bulls+"A"+cows+"B";
