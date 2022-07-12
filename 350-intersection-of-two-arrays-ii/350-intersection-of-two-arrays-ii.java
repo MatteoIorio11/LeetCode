6fe719898fe67cadb01cdb45fa7789a869ab242d
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-12 11:08:50 +0200

    Time: 5 ms (47.92%), Space: 42 MB (99.65%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map1 = new HashMap<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         
-        int[] out = new int[nums1.length];
-        for(int i = 0; i < nums1.length; i++){
-            if(!map1.containsKey(nums1[i])){
-                map1.put(nums1[i], 1);
-            }else{
-                map1.put(nums1[i], map1.get(nums1[i])+1);
-            }
+        var l = 0;
+        var r = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> out = new LinkedList<>();//int[nums1.length > nums2.length ? nums2.length : nums1.length];
+        for(int i = 0; i < nums1.length ; i++){
+            map.merge(nums1[i], 1, (x,y) -> x+y);
         }
-        
-        int index = 0;
-        int values = 0;
         for(int i = 0; i < nums2.length; i++){
-            if(map1.containsKey(nums2[i]) && map1.get(nums2[i]) > 0){
-                out[index++] = nums2[i];
-                map1.put(nums2[i], map1.get(nums2[i])-1);
-                values++;
+            if(map.containsKey(nums2[i])){
+                out.add(nums2[i]);
+                map.merge(nums2[i], 1, (x, y) -> x-y);
+                if(map.get(nums2[i]) == 0){map.remove(nums2[i]);}
             }
         }
-        return Arrays.copyOf(out, values);
+        int[] arr = new int[out.size()];
+        for(int i = 0; i < out.size(); i++){
+            arr[i] = out.get(i);
+        }
+        return arr;
     }
     
 }
