e75f993998fc717ed3ccb9c59eb369f9edee1022
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-13 11:06:51 +0200

    Time: 1 ms (99.91%), Space: 49.1 MB (92.79%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,14 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-  //two pointers TC: o(n) SC:o(1)
-    int first=0,last=s.length-1;
-    while(first<last)
-    {
-        char ch=s[first];
-        s[first]=s[last];
-        s[last]=ch;
-        first++;
-        last--;
-    }
+        int end = s.length-1;
+        int start = 0;
+        while(start < end){
+            char tmp = s[start];
+            s[start] = s[end];
+            s[end] = tmp;
+            end--;
+            start++;
+        }
     }
 }
