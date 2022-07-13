23a2fa2c2dde0f481e6bd5fa17c231ddb57a7ab5
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-13 10:58:21 +0200

    Time: 1 ms (72.34%), Space: 42 MB (39.90%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> out = new LinkedList<>();
-        for(int i = 0; i < numRows; i++){
+        for(int row = 0; row < numRows; row++){
             out.add(new LinkedList<Integer>());
-            for(int j = 0; j <= i; j++){
-                if(j == 0 || j == i){
-                    out.get(i).add(1);
+            for(int col = 0; col <= row; col++){
+                if(col == 0 || col == row){
+                    out.get(row).add(1);        
                 }else{
-                    var sum = out.get(i-1).get(j-1) + out.get(i-1).get(j);
-                    out.get(i).add(sum);
+                    var sum = out.get(row-1).get(col-1) + out.get(row-1).get(col);
+                    out.get(row).add(sum);
                 }
             }
         }
