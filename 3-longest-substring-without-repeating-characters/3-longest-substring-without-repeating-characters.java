51000540d452eaaa63e395ab646f705bd00d7cab
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-15 11:29:37 +0200

    Time: 609 ms (5.00%), Space: 203.8 MB (5.39%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,18 +5,16 @@ class Solution {
         var i = 0;
         var j = 1;
         var len = 0;
-        Map<String, Integer> map = new HashMap<>();
         while(i < s.length() && j <= s.length()){
             var sub = s.substring(i, j);
             len = sub.length();
-                if(j < s.length() && !sub.contains(String.valueOf(s.charAt(j)))){
-                    j++;
-                }else{
-                    map.put(sub, len);
-                    max = Math.max(len, max);            
-                    i+=1;
-                    j = i+1;
-                }
+            if(j < s.length() && !sub.contains(String.valueOf(s.charAt(j)))){
+                j++;
+            }else{
+                max = Math.max(len, max);            
+                i+=1;
+                j = i+1;
+            }
         }
         return Math.max(max, len);
     }
