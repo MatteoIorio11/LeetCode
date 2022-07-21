425527f095320e46ff66139a74d7a572c15cb8d0
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-21 10:55:30 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (56.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        double ways = 0;
-        for(int i = 0; i < n && (i <= n-i); i++){
-            ways+=factorial(n-i, i)/factorial(i,i);
-        }
-        return (int) ways;
-    }
-    double factorial(int n, int stop){
-        if(n <= 0 or n <= 1 or stop <= 0){
+        if(n == 1){
             return 1;
         }
-        return n*(factorial(n-1, stop-1));
+        int dp[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
