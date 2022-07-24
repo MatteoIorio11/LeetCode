38168c7bbf86e7f583f45ce270c17976b416f682
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-07-24 10:57:40 +0200

    Time: 291 ms (19.67%), Space: 14.9 MB (68.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int l = 0;
+        int r = matrix[0].size()-1;
+        while(l < matrix.size() and r >= 0){
+            if(matrix[l][r] == target){
+                return true;
+            }else if(matrix[l][r] > target){
+                r--;
+            }else if(matrix[l][r] < target){
+                l++;
+            }
+        }
+        return false;
+    }
+};
