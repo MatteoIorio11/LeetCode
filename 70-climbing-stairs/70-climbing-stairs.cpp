72303185f5206dd3d3ceac1e16a4a0473d7e6987
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-08-02 15:35:30 +0200

    Time: 3 ms (36.03%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,12 +4,12 @@ public:
         if(n == 1){
             return 1;
         }
-        int dp[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        vector<int> dp(n,0);
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[n];
+        return dp[n-1];
     }
 };
