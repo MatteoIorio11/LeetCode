83f7efcceddd93dd3091fa0fbab52de459f13f6d
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-08-31 15:21:10 +0200

    Time: 35 ms (29.62%), Space: 15.8 MB (15.00%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    unordered_set<string> dict;
+    unordered_map<string, bool> map;
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+
+        for(int i = 0; i < wordDict.size(); i++){
+            dict.insert(wordDict[i]);
+        }
+        
+        return check(s);
+    }
+    
+    bool check(string s){
+        if(dict.find(s) != dict.end()){
+            return true;
+        }
+        if(map.find(s) != map.end()){
+            return map[s];
+        }
+        string temp1 = "";
+        string temp2 = "";
+        for(int i = 0; i < s.length(); i++){
+            
+            temp1 = s.substr(0, i);
+            
+            if(dict.find(temp1) != dict.end()){
+                temp2 = s.substr(i, s.length());
+                
+                if(check(temp2)){
+                    return map[temp2] = true;
+                }
+            }
+        }
+        return map[s] = false;
+    }
+};
