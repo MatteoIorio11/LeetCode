08b24d5735e99cb609a557a819d379bc12ed0fa7
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-09-14 11:43:43 +0200

    Time: 66 ms (99.37%), Space: 27.7 MB (76.41%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
+    int m = 0, n = 0;
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (rows == 0) 
-            return matrix;
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX - 100000));
-
-        //First pass: check for left and top
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                } else {
-                    if (i > 0)
-                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
-                    if (j > 0)
-                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+        m = matrix.size();
+        n = matrix[0].size();
+        queue<pair<int,int>> q;
+        vector<pair<int,int>> adj = {{1,0}, {0,1}, {-1,0}, {0, -1}};
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX-10000));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    dp[i][j] = 0;
+                }else{
+                    if(i > 0){
+                        dp[i][j] = min(1 + dp[i-1][j], dp[i][j]); 
+                    }
+                    if(j > 0){
+                        dp[i][j] = min(1 + dp[i][j-1], dp[i][j]);
+                    }
                 }
             }
         }
-
-        //Second pass: check for bottom and right
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 0; j--) {
-                if (i < rows - 1)
-                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
-                if (j < cols - 1)
-                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+                
+        
+        for(int r = m-1; r >= 0; r--){
+            for(int c = n-1; c >= 0; c--){
+                if(r < m-1){
+                    dp[r][c] = min(1+dp[r+1][c], dp[r][c]);
+                }
+                if(c < n-1){
+                    dp[r][c] = min(1+dp[r][c+1], dp[r][c]);
+                }
             }
         }
-        return dist;
+        return dp;
     }
 };
